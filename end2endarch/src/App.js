401ff81c7dd3cea96db04ac07fa057acd45841b8
A-Rahman-19 <abdur.rahman19@outlook.com>
2025-05-17 14:03:34 +0530

    Added header and footer component

--- end2endarch/src/App.js
+++ end2endarch/src/App.js
@@ -1,13 +1,17 @@
 import "./App.css";
 import { BrowserRouter } from "react-router-dom";
 import AppRoutes from "./Components/routes";
+import Header from "./Components/header";
+import Footer from "./Components/footer";
 
 function App() {
   return (
     <div className="App">
       <div className="container">
         <BrowserRouter>
+        <Header/>
           <AppRoutes />
+          <Footer/>
         </BrowserRouter>
       </div>
     </div>
---
+++ end2endarch/src/Components/header.js
@@ -0,0 +1,11 @@
+import '../Styles/header.css';
+
+function Header() {
+    return (
+        <div className="header-container">
+            <h1>HeaderPart</h1>
+        </div>
+    );
+}
+
+export default Header;
