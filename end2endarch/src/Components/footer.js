2d94b6e53ce907460d24f5cebda5f92f81f6cd07
A-Rahman-19 <abdur.rahman19@outlook.com>
2025-05-20 11:53:19 +0530

    Footer content completed

--- end2endarch/src/Components/footer.js
+++ end2endarch/src/Components/footer.js
@@ -1,11 +1,75 @@
-import '../Styles/footer.css';
+import "../Styles/footer.css";
 
 function Footer() {
-    return (
-        <div className="footer-container">
-            <h1>HeaderPart</h1>
+  return (
+    <div className="footer-container">
+      <div className="inner-container1">
+        <img
+          src={require("../assets/End2Endlogo.png")}
+          alt="Company logo"
+          className="footer-logo"
+        />
+        <div className="policies">
+          <a className="">Terms & conditions</a>
+          <a className="">Privacy policies</a>
         </div>
-    );
+      </div>
+      <div className="inner-container2">
+        <div className="informations">
+        <div className="contact">
+          <h2>Contact us</h2>
+          <p>Phone number: 7708821267</p>
+        </div>
+        <div className="contact">
+          <h2>Location</h2>
+          <p>
+            28 logamuthu mariamman kovil street, muthialpet,<br/> Puducherry 605003
+          </p>
+        </div>
+        <div className="contact">
+          <h2>Eamil Address</h2>
+          <p>info@end2endarch.in</p>
+        </div>
+        </div>
+         <div className="informations">
+        <div className="contact">
+          <h2>Follow us on:</h2>
+          <div className="socials">
+            <a href="https://www.instagram.com/end2end_architecture/" target="_blank" rel="noopener noreferrer">
+              <img
+                // src={require("../assets/instagram.png")}
+                alt="Instagram"
+                className="social-icon"
+              />
+            </a>
+            <a href="https://www.facebook.com/end2end.architecture" target="_blank" rel="noopener noreferrer">
+              <img
+                // src={require("../assets/facebook.png")}
+                alt="Facebook"
+                className="social-icon"
+              />
+            </a>
+                <a href="https://www.facebook.com/end2end.architecture" target="_blank" rel="noopener noreferrer">
+              <img
+                // src={require("../assets/facebook.png")}
+                alt="Facebook"
+                className="social-icon"
+              />
+            </a>
+                        <a href="https://www.facebook.com/end2end.architecture" target="_blank" rel="noopener noreferrer">
+              <img
+                // src={require("../assets/facebook.png")}
+                alt="Facebook"
+                className="social-icon"
+              />
+            </a>
+          </div>
+        <p className="copyright">© End 2 End Architecteure | All Rights Reserved</p>
+        </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default Footer; 
+export default Footer;
