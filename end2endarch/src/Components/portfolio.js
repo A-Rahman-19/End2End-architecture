0f53849a33356c7b836e8e08ceb3fa5117e77b30
A-Rahman-19 <abdur.rahman19@outlook.com>
2025-05-20 16:02:08 +0530

    Portfolio page completed

--- end2endarch/src/Components/portfolio.js
+++ end2endarch/src/Components/portfolio.js
@@ -11,10 +11,41 @@ function Portfolio() {
   </div>
       </div>
       <div className="portfolio">
-        <div className="portfolio-images"></div>
-        <div className="portfolio-images"></div>
-        <div className="portfolio-images"></div>
-        <div className="portfolio-images"></div>
+        <div className="portfolio-images">
+                <img
+        src={require("../assets/portfolio4.jpg")}
+        alt="project"
+        className="project-images"
+      />
+        </div>
+        <div className="portfolio-images">
+          <img
+        src={require("../assets/portfolio2.jpg")}
+        alt="project"
+        className="project-image"
+      />
+        </div>
+        <div className="portfolio-images">
+          <img
+        src={require("../assets/portfolio1.jpg")}
+        alt="project"
+        className="project-image"
+      />
+        </div>
+        <div className="portfolio-images">
+          <img
+        src={require("../assets/portfolio3.jpg")}
+        alt="project"
+        className="project-image"
+      />
+        </div>
+           <div className="portfolio-images">
+          <img
+        src={require("../assets/portfolio3.jpg")}
+        alt="project"
+        className="project-image"
+      />
+        </div>
       </div>
     </div>
   );
